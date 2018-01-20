2082445b9698cee0a08067a0fcec58e14213b795
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-20 07:23:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,5 +24,6 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  return delete object[key];
+  delete object[key];
+  return object
 }
